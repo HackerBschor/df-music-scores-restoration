e067ca7b6dbfa62fadaea08eb15ccc74a1c85649
Nicolas Bschor <nico.bschor@gmail.com>
2023-11-09 17:26:10 +0100

    frist model and training

--- dl/util.py
+++ dl/util.py
@@ -5,24 +5,40 @@ import numpy as np
 
 def undo_preprocessing(image_tensor: torch.Tensor) -> np.ndarray:
     image_numpy = image_tensor.numpy()
-    image_tensor_transpose = image_numpy.transpose((1, 2, 0))
 
     # Undo preprocessing
-    mean = np.array([0.5, 0.5, 0.5])
-    std = np.array([0.5, 0.5, 0.5])
-    image = std * image_tensor_transpose + mean
+    mean = np.array(0.5)
+    std = np.array(0.5)
+    image = std * image_numpy + mean
 
-    return np.clip(image, 0, 1)
+    return image
 
 
-def show_clean_dirty_images(tensor_img_clean: torch.Tensor, tensor_img_dirty: torch.Tensor) -> None:
+def show_clean_dirty_images(tensor_img_dirty: torch.Tensor, tensor_img_clean: torch.Tensor) -> None:
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 10))
-    img_1 = undo_preprocessing(tensor_img_clean)
-    img_2 = undo_preprocessing(tensor_img_dirty)
+    img_1 = undo_preprocessing(tensor_img_dirty)
+    img_2 = undo_preprocessing(tensor_img_clean)
 
-    ax1.imshow(img_1)
-    ax1.set_title('Clean Image')
-    ax2.imshow(img_2)
-    ax2.set_title('Dirty Image')
+    ax1.imshow(img_1.squeeze(), cmap='gray')
+    ax1.set_title('Dirty Image')
+    ax2.imshow(img_2.squeeze(), cmap='gray')
+    ax2.set_title('Clean Image')
+
+    plt.show()
+
+
+def show_clean_dirty_prediction_images(
+        tensor_img_dirty: torch.Tensor, tensor_img_clean: torch.Tensor, tensor_img_prediction: torch.Tensor) -> None:
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 10))
+    img_1 = undo_preprocessing(tensor_img_dirty)
+    img_2 = undo_preprocessing(tensor_img_clean)
+    img_3 = undo_preprocessing(tensor_img_prediction)
+
+    ax1.imshow(img_1.squeeze(), cmap='gray')
+    ax1.set_title('Dirty Image')
+    ax2.imshow(img_2.squeeze(), cmap='gray')
+    ax2.set_title('Clean Image')
+    ax3.imshow(img_3.squeeze(), cmap='gray')
+    ax3.set_title('Prediction Image')
 
     plt.show()
