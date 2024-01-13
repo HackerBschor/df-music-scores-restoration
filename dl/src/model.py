6cf78a367dae73508e1277c4f6bfd329240ed1d0
Nicolas Bschor <nico.bschor@gmail.com>
2024-01-13 14:32:17 +0100

    added model in src

---
+++ dl/src/model.py
@@ -0,0 +1,31 @@
+from torch import nn
+
+
+class ConvAutoencoderDenoiser(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # defining the encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(32, 1, kernel_size=3, padding=1, stride=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
